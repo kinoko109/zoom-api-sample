4232e0ed7694f78cd30ce4d7ddb20d0a7646d37b
kinoko109 <tatsuaki1009@gmail.com>
2024-09-18 12:14:26 +0900

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,58 @@
 "use client";
 import { useEffect, useState } from "react";
+import JsonFormatter from "react-json-formatter";
+
+const jsonStyle = {
+  propertyStyle: { color: "red" },
+  stringStyle: { color: "green" },
+  numberStyle: { color: "darkorange" },
+};
 
 export default function Home() {
-  // const [message, setMessage] = useState();
-  //
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await fetch("/api/get-zoom-token");
-  //     const { message } = await res.json();
-  //     setMessage(message);
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // if (!message) return <p>Loading...</p>;
+  const [response, setResponse] = useState();
 
   const handleClickToken = async () => {
     const res = await fetch("/api/get-zoom-token");
     const data = await res.json();
     console.log("handleClickToken", data);
+    setResponse(data);
   };
 
   const handleClickRecordList = async () => {
     const res = await fetch("/api/me");
     const data = await res.json();
     console.log("handleClickRecordList", data);
+    setResponse(data);
   };
 
   return (
-    <>
-      <button type="button" onClick={handleClickToken}>
-        get token
-      </button>
-      <button type="button" onClick={handleClickRecordList}>
-        get me
-      </button>
-    </>
+    <main className="p-14 flex">
+      <div className="flex-1">
+        <div>
+          <button
+            type="button"
+            className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110"
+            onClick={handleClickToken}
+          >
+            get token
+          </button>
+          <p>https://zoom.us/oauth/token</p>
+        </div>
+        <div className="mt-4">
+          <button
+            type="button"
+            className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110"
+            onClick={handleClickRecordList}
+          >
+            get me
+          </button>
+          <p>https://api.zoom.us/v2/users/me</p>
+        </div>
+      </div>
+
+      <div className="p-10 flex-1 border-2 h-screen w-[50%] break-words overflow-auto">
+        {<JsonFormatter json={response} tabWith={4} jsonStyle={jsonStyle} />}
+      </div>
+    </main>
   );
 }
